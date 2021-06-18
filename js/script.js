cb246090da0a0192dd60bba3cb9d65961b033f51
Marco Tessitore <marcotexcode@gmail.com>
2021-06-18 17:04:20 +0200

    modifica funzione

--- js/script.js
+++ js/script.js
@@ -5,20 +5,27 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
+const numeri = [1,2,3,4,5,6,7,8,9,];
+const a = 2;
+const b = 7;
 
 
 
-function rage() {
 
-    const numeri = [1,2,3,4,5,6,7,8,9,];
-    const a = 2;
-    const b = 7;
+
+let range = rage(numeri,a,b);
+
+
+
+
+
+function rage(arr,var1 , var2) {
 
     const numeriRange = [];
 
-    numeri.forEach((element,index) => {
+    arr.forEach((element,index) => {
 
-        if (index >= a && index <= b) {
+        if (index >= var1 && index <= var2) {
 
             numeriRange.push( element);
 
@@ -29,6 +36,6 @@ function rage() {
     return numeriRange;
 }
 
-    console.log(rage());
+
 
 
